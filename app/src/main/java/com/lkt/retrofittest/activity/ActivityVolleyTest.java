badfe4fb5a8ae8c9b4dc982a5e536329de2a0a2d
LiKunTao <742975145@qq.com>
2018-08-08 10:05:05 +0800

    测试：修改参数

--- app/src/main/java/com/lkt/retrofittest/activity/ActivityVolleyTest.java
+++ app/src/main/java/com/lkt/retrofittest/activity/ActivityVolleyTest.java
@@ -1,21 +1,16 @@
 package com.lkt.retrofittest.activity;
 
-import android.app.DownloadManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.lkt.retrofittest.R;
 import com.lkt.retrofittest.interfacer.IOnResponse;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActivityVolleyTest extends BaseActivity {
     @Override
@@ -23,22 +18,8 @@ public class ActivityVolleyTest extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volley);
         final TextView textView = findViewById(R.id.textView);
-//        RequestQueue requestQueue = Volley.newRequestQueue(this);
-//        Request request = new StringRequest("https://www.baidu.com/", new Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//                textView.setText(Html.fromHtml(response));
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//
-//            }
-//        });
-//        requestQueue.add(request);
-//        requestQueue.start();
-
-        request("https://www.baidu.com/", new IOnResponse() {
+        Map<String, String> map = new HashMap<>();
+        request(map, "https://www.baidu.com/", new IOnResponse() {
             @Override
             public void onSuccess(String response) {
                 textView.setText(Html.fromHtml(response));
